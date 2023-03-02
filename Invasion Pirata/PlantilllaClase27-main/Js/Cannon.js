304c37f181ada29b188a72275e179d2bfe40a216
Gabs272 <121832829+Gabs272@users.noreply.github.com>
2023-03-01 20:00:22 -0500

    Add files via upload

---
+++ Invasion Pirata/PlantilllaClase27-main/Js/Cannon.js
@@ -0,0 +1,30 @@
+class Cannon {
+    constructor(x, y, width, height, angle){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.angle = angle;
+    }
+
+    display() {
+
+        if(keyIsDown(UP_ARROW) && this.angle < 0.25) { 
+            this.angle += 0.02; 
+            console.log(this.angle); 
+        } 
+        if(keyIsDown(DOWN_ARROW) && this.angle > -1.50) { 
+            this.angle -= 0.02; 
+            console.log(this.angle); 
+        }
+
+        fill("grey")
+        push();
+        translate(this.x, this.y);
+        rotate(angle);
+        rect(-10, -20, this.width, this.height);
+        pop();
+        arc(this.x -30 , this.y +90, 140, 200, PI, TWO_PI);
+        noFill();
+    }
+}
